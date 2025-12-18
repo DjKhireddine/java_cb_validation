ebdc88748e9e175b51aedfaf7964c46a6e0eeb70
Djamel KHIREDDINE <dj.khireddine@gmail.com>
2025-12-18 15:18:48 +0100

    add validate cb function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
-            System.out.println("Hello");
+
+        //System.out.println(validateCB("4242424242424242"));
+        System.out.println(validateCB("5356810197874515"));
+    }
+
+    public static boolean validateCB(String cbNumber)
+    {
+        if (cbNumber.length() != 16) {
+            return false;
+        }
+
+        String[] splitted = cbNumber.split("");
+        System.out.println(Arrays.toString(splitted));
+
+        int[] handled = new int[16];
+
+        for (int i=0; i<splitted.length; i++) {
+            int number = Integer.parseInt(splitted[i]);
+            if (i%2 == 0) {
+                int doubleX = number*2;
+                doubleX = number == 9 ? 9 : doubleX%9;
+                handled[i] = doubleX;
+            } else {
+                handled[i] = number;
+            }
+        }
+
+        int sum = IntStream.of(handled).sum();
+
+        return sum % 10 == 0;
     }
 }
