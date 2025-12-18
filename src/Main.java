e3de2253d9f487e8872eefbd56f30919dbd8ca4f
Djamel KHIREDDINE <dj.khireddine@gmail.com>
2025-12-18 15:38:48 +0100

    update accept cb with max 16 numbers

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,25 @@ import java.util.stream.IntStream;
 public class Main {
     public static void main(String[] args) {
 
-        //System.out.println(validateCB("4242424242424242"));
-        System.out.println(validateCB("5356810197874515"));
+        System.out.println(validateCB("4242424242424242"));
     }
 
     public static boolean validateCB(String cbNumber)
     {
-        if (cbNumber.length() != 16) {
+        if (cbNumber.length() > 16) {
             return false;
         }
 
         String[] splitted = cbNumber.split("");
         System.out.println(Arrays.toString(splitted));
 
-        int[] handled = new int[16];
+        int[] handled = new int[cbNumber.length()];
 
         for (int i=0; i<splitted.length; i++) {
             int number = Integer.parseInt(splitted[i]);
-            if (i%2 == 0) {
+            // On double en commençant du premier chiffre si le nombre de chiffres est pair sinon par le deuxième
+            int rest = (cbNumber.length() % 2 == 0) ? 0 : 1;
+            if (i%2 == rest) {
                 int doubleX = number*2;
                 doubleX = number == 9 ? 9 : doubleX%9;
                 handled[i] = doubleX;
